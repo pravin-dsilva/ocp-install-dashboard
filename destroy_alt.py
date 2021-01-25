479b5f7e5f3b1af0d48d0764c9fb032e32aef694
Prajyot Parab <prajot.parab@ibm.com>
2021-01-25 08:27:44 -0800

    code update
    Signed-off-by: Prajyot Parab <prajot.parab@ibm.com>

--- destroy_alt.py
+++ destroy_alt.py
@@ -1,11 +1,16 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-def job_trigger(bn,user):
+def job_trigger(bn,user,platform):
     username = 'shilpi12@in.ibm.com'
     token = '<token>'
 
-    post_url = 'http://molly.aus.stglabs.ibm.com:8090/job/demo-powervc-job-with-parameters/{}/doDelete'.format(bn)
+    if platform == 'powervs':
+       job = 'demo-powervs-job-with-parameters'
+    else:
+       job = 'demo-powervc-job-with-parameters'
+
+    post_url = 'http://molly.aus.stglabs.ibm.com:8090/job/{}/{}/doDelete'.format(job,bn)
     post_response = requests.post(post_url, auth=HTTPBasicAuth(username, token))
     print(post_response.status_code)
 
